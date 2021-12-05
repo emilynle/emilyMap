487c3ad24f790b4a16371f672d28e30e10ec44e4
darcyhughes <hughes.darcy123@gmail.com>
2021-12-05 10:13:19 -0800

    added css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,108 +1,116 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import L from "leaflet";
-import { Marker, useMap } from "react-leaflet";
-import axios from 'axios';
-
-import { promiseToFlyTo, getCurrentLocation } from "lib/map";
-
+import { useMap } from "react-leaflet";
+import axios from "axios";
 import Layout from "components/Layout";
 import Container from "components/Container";
 import Map from "components/Map";
 import Snippet from "components/Snippet";
-
-import gatsby_astronaut from "assets/images/gatsby-astronaut.jpg";
-
 const LOCATION = {
   lat: 38.9072,
   lng: -77.0369,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
-
-/**
- * MapEffect
- * @description This is an example of creating an effect used to zoom in and set a popup on load
- */
-
-const MapEffect = ({ markerRef }) => {
-  const map = useMap();
-
-  useEffect = async() => {
+const IndexPage = () => {
+  // const markerRef = useRef();
+  async function mapEffect(map) {
     let response;
-
     try {
-      response = await axios.get('https://corona.lmao.ninja/v2/countries');
-    } catch(e) {
-      console.log(`Failed to fetch countries: ${e.message}`, e);
-      return;
+      response = await axios.get("https://corona.lmao.ninja/v2/countries");
+      console.log("response", response);
+    } catch (error) {
+      console.log("error", "failed to fetch countries");
+      return null;
     }
 
     const { data = [] } = response;
+
     const hasData = Array.isArray(data) && data.length > 0;
 
-    if ( !hasData ) return;
+    if (!hasData) return;
 
     const geoJson = {
-      type: 'FeatureCollection',
+      type: "FeatureCollection",
       features: data.map((country = {}) => {
         const { countryInfo = {} } = country;
         const { lat, long: lng } = countryInfo;
         return {
-          type: 'Feature',
+          type: "Feature",
           properties: {
-          ...country,
+            ...country,
           },
           geometry: {
-            type: 'Point',
-            coordinates: [ lng, lat ]
-          }
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`;
         }
-      })
-    }
-  }, [map, markerRef]);
 
-  return null;
-};
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
 
-const IndexPage = () => {
-  const markerRef = useRef();
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
 
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: "icon",
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo(map);
+
+    return null;
+  }
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: "OpenStreetMap",
     zoom: DEFAULT_ZOOM,
+    whenCreated: mapEffect,
   };
-
   return (
     <Layout pageName="home">
       <Helmet>
         <title>Home Page</title>
       </Helmet>
-
       <Map {...mapSettings}>
-        <MapEffect markerRef={markerRef} />
-        <Marker ref={markerRef} position={CENTER} />
+        {/* <MapEffect markerRef={markerRef} /> */}
+        {/* <Marker ref={markerRef} position={CENTER} /> */}
       </Map>
-
       <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
@@ -117,5 +125,4 @@ const IndexPage = () => {
     </Layout>
   );
 };
-
 export default IndexPage;
